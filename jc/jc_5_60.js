c6ff9f38efd6b0ba4525e4be481975b98348d414
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-11-19 11:06:21 +0800

    Update jc_5_60.js

--- jc/jc_5_60.js
+++ jc/jc_5_60.js
@@ -1,3 +1,9 @@
+/*
+修改Jxell-qq:15717980  zq_5_60.js
+软件名称：晶彩看点
+重写链接https://ant.xunsl.com/v17/Ad/getReward.json  对应目标   https://ghproxy.com/https://github.com/liuqi6968/-/blob/main/Jc_5_60.js
+*/
+
 const $ = new Env('晶彩看点5分钟60分钟任务领取');
 let jcerciboxbody= $.isNode() ? (process.env.jcerciboxbody ? process.env.jcerciboxbody : "") : ($.getdata('jcerciboxbody') ? $.getdata('jcerciboxbody') : "")
 let jcboxbodyArr = []
